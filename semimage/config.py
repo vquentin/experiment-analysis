688f0160b78248b143ba665593703df8ba190882
Quentin Van Overmeere <49268972+vquentin@users.noreply.github.com>
2021-02-11 13:24:14 +0100

    Reorganized repo

--- semimage/config.py
+++ semimage/config.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021, Quentin Van Overmeere
 # Licensed under MIT License
 
-#Colors through which to cycle for graphs (R, G, B)
+# Colors through which to cycle for graphs (R, G, B)
 colors = ((214, 39, 40), (31, 119, 180), (255, 127, 14), (44, 160, 44), (148, 103, 189), (227, 119, 194), (188, 189, 34), (23, 190, 207))
