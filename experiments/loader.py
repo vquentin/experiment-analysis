Corrected distance calculation bug

experiments/loader.py
@@ -101,16 +101,14 @@ class UniformitySEMCS(Experiment):
             if file_candidate.exists():
                 self._result.append(pd.read_csv(file_candidate))
             else:
-                result_image = np.zeros((len(sample_path), 4))
-                for i, image_file in enumerate(sample_path):
+                result_image = []
+                for image_file in sample_path:
                     plt.show()
-                    result_image[i, :] = ia.get_porous_thickness(SEMZeissImage(image_file))  # x, y, thickness, uncertainty
-                idx = np.argmin(result_image[:,0])
-                d = np.linalg.norm(result_image[:, 0:1] - result_image[idx, 0:1], axis=1)
-                d = d-np.min(d)
-                result = np.concatenate((result_image, d[:, np.newaxis]), axis=1)
-                result = result[result[:,0].argsort()]
-                df = pd.DataFrame(result, columns=['X [mm]', 'Y [mm]', 'Porous thickness [um]', 'Uncertainty [um]', 'Distance [mm]'])
+                    result_image.append(ia.get_porous_thickness(SEMZeissImage(image_file)))
+                df = pd.DataFrame(result_image, columns=['Image', 'X [mm]', 'Y [mm]', 'Porous thickness [um]', 'Uncertainty [um]'])
+                xy = df.iloc[:, df.columns.get_indexer(['X [mm]', 'Y [mm]'])].to_numpy()
+                d = np.linalg.norm(xy - xy[0, :], axis=1) 
+                df['Distance [mm]'] = d
                 self._result.append(df)
 
     def plot(self, legend):
@@ -142,7 +140,9 @@ class UniformitySEMCSNormalize(UniformitySEMCS):
             distFirstCavity = 6400  # um
             result['Distance to collector [mm]'] = abs(abs((result['Distance [mm]']+distFirstCavity/1000)-(distCenterCollector/1000))-distCenterCollector/1000)
             #TODO normalize thickness result['Ratio ']
-            result['Thickness Ratio Center [-]'] = result['Porous thickness [um]']/min(result['Porous thickness [um]'])
+            log.debug(result.dtypes)
+            log.debug(result.loc[:, 'Porous thickness [um]'])
+            result['Thickness Ratio Center [-]'] = result.loc[:, 'Porous thickness [um]'] / min(result.loc[:, 'Porous thickness [um]'])
 
     def plot(self, legend):
         fig = plt.figure()

semimage/image_analysis.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021, Quentin Van Overmeere
 # Licensed under MIT License
 
+from os import name
 import numpy as np
 from tifffile import TiffFile as tf
 from matplotlib import pyplot as plt
@@ -35,18 +36,23 @@ from skimage.segmentation import (morphological_chan_vese,
                                   checkerboard_level_set)
 
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
 def get_porous_thickness(sem_image):
     edges = edges_filter(sem_image, show=False)
     edges_sides = edges_on_side(edges, show=False, image=sem_image)
     lines = find_lines(mask_center_h(edges_sides, 0.5),
                        show=False, image=sem_image)
-    features = classify_lines(lines, show=True, image=sem_image)
+    features = classify_lines(lines, show=False, image=sem_image)
     porous_thickness = measure_porous_thickness(features, edges, edges_sides, show=False, image=sem_image)
+    result = [sem_image.image_name, sem_image.metadata.stage_x, sem_image.metadata.stage_y, np.nan, np.nan]
     if porous_thickness is not None:
-        print(*porous_thickness)
-        return np.array([sem_image.metadata.stage_x, sem_image.metadata.stage_y, *porous_thickness])
-    else:
-        return np.array([sem_image.metadata.stage_x, sem_image.metadata.stage_y, np.nan, np.nan])
+        log.debug(porous_thickness)
+        result[3] = porous_thickness[0]
+        result[4] = porous_thickness[1]
+    return result
 
 
 def __overlay(*args):