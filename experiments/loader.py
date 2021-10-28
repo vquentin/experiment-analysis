Corrected cavity depth calculation, corrected results caching and other features

experiments/loader.py
@@ -9,7 +9,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import itertools
-from cycler import cycler
 
 from semimage.sem_image import SEMZeissImage
 import semimage.image_analysis as ia
@@ -27,8 +26,7 @@ except ImportError:
     log.warning("Could not import yaml.CLoader")
 
 # load sample descriptions
-path = Path(__file__)
-samples_description = yaml.safe_load(open(path.parent / 'samples.yml'))
+samples_description = yaml.safe_load(open(Path(__file__).parent / 'samples.yml'))
 
 # constant variables for experiment types
 UNIFORMITYSEMCS = 'uniformity-SEM-CS'
@@ -52,13 +50,15 @@ class Experiment(object):
     def __init__(self, experiment_id, *samples):
         self._type = experiment_id
         self._samples = samples
-        print(samples)
-        print(*samples)
+        self._sample_paths = [glob.glob(sample) if sample is not None else None
+                              for sample in self.get_path(*samples)]
+        self._result = []
+        log.debug(samples)
 
     def run(self):
         raise NotImplementedError
 
-    def plot(self):
+    def plot(self, legend):
         raise NotImplementedError
 
     def save(self):
@@ -89,15 +89,12 @@ class UniformitySEMCS(Experiment):
 
     def __init__(self, *samples, **ignored_kwargs):
         super().__init__(UNIFORMITYSEMCS, *samples)
-        self._sample_paths = [glob.glob(sample) if sample is not None else None
-                              for sample in self.get_path(*samples)]
-        self._result = []
+        self.__cache_name = "result.csv"
 
     def run(self):
         for sample_path in self._sample_paths:
             path = Path(sample_path[0])
-            name = "result.csv"
-            file_candidate = path.parent / name
+            file_candidate = path.parent / self.__cache_name
             if file_candidate.exists():
                 self._result.append(pd.read_csv(file_candidate))
             else:
@@ -105,11 +102,12 @@ class UniformitySEMCS(Experiment):
                 for image_file in sample_path:
                     plt.show()
                     result_image.append(ia.get_porous_thickness(SEMZeissImage(image_file)))
-                df = pd.DataFrame(result_image, columns=['Image', 'X [mm]', 'Y [mm]', 'Porous thickness [um]', 'Uncertainty [um]'])
-                xy = df.iloc[:, df.columns.get_indexer(['X [mm]', 'Y [mm]'])].to_numpy()
+                result = pd.DataFrame(result_image, columns=['Image', 'X [mm]', 'Y [mm]', 'Porous thickness [um]', 'Uncertainty [um]'])
+                xy = result.iloc[:, result.columns.get_indexer(['X [mm]', 'Y [mm]'])].to_numpy()
                 d = np.linalg.norm(xy - xy[0, :], axis=1) 
-                df['Distance [mm]'] = d
-                self._result.append(df)
+                result['Distance [mm]'] = d
+                result.sort_values('Distance [mm]', inplace=True, ignore_index=True)
+                self._result.append(result)
 
     def plot(self, legend):
         fig = plt.figure()
@@ -118,39 +116,34 @@ class UniformitySEMCS(Experiment):
             result.plot(x='Distance [mm]', y='Porous thickness [um]', ax=fig.gca())
         plt.legend(self.get_legend(legend_struct=legend))
         plt.xlabel('Distance [mm]')
-        plt.ylabel('Porous Si thickness [um]')
+        plt.ylabel('Porous Si thickness ['+chr(956)+'m]')
         plt.show()
 
     def save(self):
         for i, result in enumerate(self._result):
             path = Path(self._sample_paths[i][0])
-            name = "result.csv"
-            result.to_csv(path.parent / name)
+            result.to_csv(path.parent / self.__cache_name, index=False)
 
 
 class UniformitySEMCSNormalize(UniformitySEMCS):
-    def __init__(self, *samples, **ignored_kwargs):
-        super().__init__(*samples, **ignored_kwargs)
 
     def run(self):
         super().run()
         for result in self._result:
             # transform points to distance from edge
-            distCenterCollector = 26500  # um
-            distFirstCavity = 6400  # um
-            result['Distance to collector [mm]'] = abs(abs((result['Distance [mm]']+distFirstCavity/1000)-(distCenterCollector/1000))-distCenterCollector/1000)
+            distance_center_collector = 26.500  # mm
+            distance_first_cavity = 6.400  # mm
+            result['Distance to collector [mm]'] = abs(abs((result['Distance [mm]']+distance_first_cavity)-(distance_center_collector))-distance_center_collector)
             #TODO normalize thickness result['Ratio ']
-            log.debug(result.dtypes)
-            log.debug(result.loc[:, 'Porous thickness [um]'])
             result['Thickness Ratio Center [-]'] = result.loc[:, 'Porous thickness [um]'] / min(result.loc[:, 'Porous thickness [um]'])
 
     def plot(self, legend):
         fig = plt.figure()
         colors = itertools.cycle(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'])
-        #cycler('color', ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']))
+        styles = itertools.cycle(['-o', '-s', '-+', '-D'])
         for result in self._result:
+            result.plot.line(x='Distance to collector [mm]', y='Porous thickness [um]', ax=fig.gca(), style=next(styles), logy=False, color=next(colors))
             #plt.errorbar(result[:,4], result[:,2], yerr=result[:,3])
-            result.plot(x='Distance to collector [mm]', y='Porous thickness [um]', ax=fig.gca(), kind='scatter', logy=False, color=next(colors))
             #result.plot(x='Distance to collector [mm]', y='Thickness Ratio Center [-]', ax=fig.gca(), kind='scatter', logy=False, color=next(colors))
         plt.legend(self.get_legend(legend_struct=legend))
         plt.xlabel('Distance from current collector [mm]')
@@ -158,14 +151,6 @@ class UniformitySEMCSNormalize(UniformitySEMCS):
         fig.gca().set_xlim(xmin=0)
         if fig.gca().get_yscale() is 'linear':
             fig.gca().set_ylim(ymin=0)
-        #for result in self._result:
-            #A, K = fit_exp_linear(result['Distance to collector [mm]'], result['Porous thickness [um]'], 0)
-            #fit_y = model_func(result['Distance to collector [mm]'], A, K, 0)
-            #fig.gca().plot(result['Distance to collector [mm]'], fit_y, 'blue', linewidth=1)
-
-            #p = np.polyfit(result['Distance to collector [mm]'], result['Porous thickness [um]'], 3)
-            #fig.gca().plot(result['Distance to collector [mm]'], np.polyval(p, result['Distance to collector [mm]']), 'blue', linewidth=1)
-
         plt.show()
 
 
@@ -173,14 +158,12 @@ class UvsT(Experiment):
 
     def __init__(self, *samples, **ignored_kwargs):
         super().__init__(UVST, *samples)
-        self._sample_paths = self.get_path(*samples)
-        self._result = []
 
     def run(self):
         for sample_path in self._sample_paths:
             plt.show()
             self._result.append(
-                ec.get_U_vs_t(sample_path))
+                ec.get_U_vs_t(*sample_path))
 
     def plot(self, legend):
         fig = plt.figure()
@@ -196,7 +179,6 @@ class Moss(Experiment):
 
     def __init__(self, *samples, **ignored_kwargs):
         super().__init__(MOSS, *samples)
-        self._sample_paths = self.get_path(*samples)
         self._sample_times = self.get_arg(*samples, key='timing')
         self._result = []
 

semimage/image_analysis.py
@@ -225,18 +225,22 @@ def classify_lines(lines, show=False, image=None):
             features[line.classify(5)] = line
     # TODO: add an if clause to reclassify porous_void as si_void if it's more than 100 um away from cavity
     if show:
-        plt.figure()
-        plt.imshow(image.image, cmap=cm.gray, vmin=0, vmax=255)
-        cavity = features.get('Cavity')
-        si_void = features.get('Si/void interface')
-        porous_void = features.get('Porous Si/void interface')
-        if cavity is not None:
-            cavity.show2(c='r')
-        if si_void is not None:
-            si_void.show2(c='b')
-        if porous_void is not None:
-            porous_void.show2(c='g')
+        show_lines(features, image=image)
     return features
+
+
+def show_lines(features, image=None):
+    plt.figure()
+    plt.imshow(image.image, cmap=cm.gray, vmin=0, vmax=255)
+    cavity = features.get('Cavity')
+    si_void = features.get('Si/void interface')
+    porous_void = features.get('Porous Si/void interface')
+    if cavity is not None:
+        cavity.show2(c='r')
+    if si_void is not None:
+        si_void.show2(c='b')
+    if porous_void is not None:
+        porous_void.show2(c='g')
 
 
 def classify(self, lines=None, debug=False):
@@ -280,7 +284,7 @@ def measure_porous_thickness(features, edges, edges_sides, show=False, image=Non
     if 'Si/void interface' in features:
         return measure_porous_thickness_flat(features['Si/void interface'], edges, edges_sides, show=show, image=image)
     else:
-        return 0, 0
+        return None
 
 
 def measure_porous_thickness_flat(interface, edges, edges_sides, show=False, image=None):
@@ -295,17 +299,26 @@ def measure_porous_thickness_cavity(cavity, edges, edges_sides, show=False, imag
     side_cavity = cavity.side
     side_porous = math.floor(side_cavity/2)*2+(side_cavity+1) % 2
     cavity_depth = cavity.distance_to_edge_exclude_zero_um(edges_sides[..., side_cavity], show=False)
-    porous_depth = cavity.distance_to_edge_um(edges_sides[..., side_porous], show=False)
+    porous_depth = cavity.distance_to_edge_um(edges_sides[..., side_porous], show=show)
     porous_thickness = porous_depth[0]-cavity_depth[0]
     #unc = porous_thickness * ((porous_depth[1]/porous_depth[0])**2+(cavity_depth[1]/cavity_depth[0])**2)**0.5
     unc = (porous_depth[1]**2+cavity_depth[1]**2)**0.5
+    log.debug(f"Cavity depth is {cavity_depth[0]} \u00B1 {cavity_depth[1]} {chr(956)}m")
+    if show:
+        show_lines({'Cavity': cavity}, image=image)
+        show_edges(edges_sides[..., side_cavity], image=image, overlay=True)
+        show_edges(edges_sides[..., side_porous], image=image, overlay=True)
     return porous_thickness, unc
 
 
-
-
-
-
+def show_edges(edges, image=None, overlay=True):
+    if isinstance(image, SEMZeissImage):
+        image = image.image
+    else:
+        image = np.ones(edges.shape)
+    if overlay:
+        __plt_overlay(image, edges, axes=plt.gca(),
+                        title=None)
 
 
 def analyze(self, analyses=None):

semimage/line.py
@@ -186,10 +186,13 @@ class Line(object):
         """
         Return the intensity difference between line at other side of current
         line, and line at same side, separated by 2*distance.
+        TODO make more robust if lines are not the same length, e.g. errors:
+            return line_minus.intensity - line_plus.intensity
+            ValueError: operands could not be broadcast together with shapes (1006,) (1024,)
         """
         line_minus, line_plus = self.lines_at_offset(distance=distance)
         if self.side == 0:
-            return line_plus.intensity-line_minus.intensity
+            return line_plus.intensity - line_minus.intensity
         elif self.side == 1:
             return line_minus.intensity - line_plus.intensity
 
@@ -266,6 +269,7 @@ class Line(object):
         if show:
             plt.figure()
             plt.plot(dist)
+        #return abs(np.max(dist)), np.std(dist)
         return abs(np.median(dist)), np.std(dist)
 
     def distance_to_points(self, p):
@@ -285,8 +289,9 @@ class Line(object):
         """Return the median and deviation of distances to line point by point, excluding zeros."""
         idx = np.column_stack(np.where(edge))
         dist = self.distance_to_points(idx)*self.__image.metadata.pixel_size/1000
-        dist_excl_zeros = dist[~np.isclose(dist, 0, rtol=0, atol=0.1)]
+        dist_excl_zeros = dist[~np.isclose(dist, 0, rtol=0, atol=0.9*max(dist))]
         if show:
             plt.figure()
             plt.plot(dist)
+            plt.plot(dist_excl_zeros)
         return abs(np.median(dist_excl_zeros)), np.std(dist_excl_zeros)